43e13b07e485e5afe9968163971c8b403bc2a69f
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-10 01:33:21 -0300

    rendered App

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+import App from "./components/App.jsx";
+
+import "./assets/reset.css";
+import './assets/body-style.css';
+
+ReactDOM.render(<App />, document.querySelector('.root'));
